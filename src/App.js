3cd8d77308bca302c7e0e55ac84822029c6bf469
khalid-akhss19 <khalid.ghulbi22@gmail.com>
2023-08-08 13:04:37 +0500

    menu project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
+function App() {
+  const [menuItems, setMenuItems] = useState();
+ 
+  return 
+  <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Menu menuItems={menuItems} /> 
+      </section>
+    </main>
+}
+
+export default App;
